f612338e3b46ceef5eac919f95a9e0d33cee1ab4
maximilianohermosilla <maximiliano_hermosilla@hotmail.com>
2021-05-22 19:04:22 -0300

    Practica 4 - Ejercicio 2

---
+++ Practica/AlumnoMuyEstudioso.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practica
+{
+	
+	// Practica 4 - Ejercicio 2 //
+	
+	public class AlumnoMuyEstudioso: Alumno
+	{
+		public AlumnoMuyEstudioso()
+		{
+		}
+		
+		public new int responderPregunta(int pregunta){
+			return (pregunta%3);
+		}
+	}
+}
